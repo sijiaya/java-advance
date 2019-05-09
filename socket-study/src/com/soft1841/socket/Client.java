c2a3c2304ec8de49f8d09fa7496ef87a00742ae1
sijiaya <2317321437@qq.com>
2019-05-10 06:59:31 +0800

    Java编程：UDP协议案例

--- socket-study/src/com/soft1841/socket/Client.java
+++ socket-study/src/com/soft1841/socket/Client.java
@@ -9,7 +9,7 @@ import java.net.Socket;
 public class Client {
     public static void main(String[] args) throws IOException {
         //创建客户端对象，指定连续的服务器IP和端口（host:主机）
-        Socket socket = new Socket("39.96.182.225",8080);
+        Socket socket = new Socket("10.30.172.54",8080);
         System.out.println("已和服务器创建连接，远程主机地址："+socket.getRemoteSocketAddress());
     }
 }
